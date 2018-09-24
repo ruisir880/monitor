f3c385a46754048df90b787ff919772a3a911604
rui <609333898@qq.com>
2018-09-24 21:45:49 +0800

    Color work.

--- script/monitorDDL3.sql
+++ script/monitorDDL3.sql
@@ -1,2 +1,1 @@
-create table monitor_point_email ( mp_id bigint not null , emailList varchar(1000));
-ALTER TABLE monitor_point_email add constraint UQ_monitor_point_email unique (mp_id);
+alter table monitor_point add email_List varchar(1000);
